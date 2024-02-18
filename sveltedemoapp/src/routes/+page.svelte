8e4df876fa88c1e86f8d316c90bd8c3735e2fce7
Ian Marshall <ian.js.tutor@gmail.com>
2024-02-18 11:52:25 -0500

    put api call into api folder

--- sveltedemoapp/src/routes/+page.svelte
+++ sveltedemoapp/src/routes/+page.svelte
@@ -2,11 +2,15 @@
     let affirmation = $state("Hello!");
 
     async function getAffirmation() {
-        const response = await fetch("./");
+        const response = await fetch("./api/");
         const content = await response.json();
         affirmation = content.affirmation;
     }
 </script>
+
+<style>
+    h1 { color: blue; }
+</style>
 
 <h1>{affirmation}</h1>
 <button on:click={getAffirmation}>Get new affirmation</button>
